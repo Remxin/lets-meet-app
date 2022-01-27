00f31e424e20bd28a0c27712b6d91129ca2ec0ee
Pawior <pawiorx@gmail.com>
2022-01-27 16:03:56 +0100

    zabawy tailwindem, zmiana navbara

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return <div className="home"> Home page</div>;
 };
 
 export default Home;
--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
-
+import { Tab } from "@headlessui/react";
+import { Switch } from "@headlessui/react";
+import { IoIosMenu } from "react-icons/io";
+import { FcLandscape } from "react-icons/fc";
 type navbarProps = {
   logged: boolean;
 };
 const Navbar = ({ logged }: navbarProps) => {
+  const [enabled, setEnabled] = useState(false);
+  let [open, setOpen] = useState(false);
+
   // console.log(logged);
   const logout = async () => {
     try {
@@ -37,23 +44,53 @@ const Navbar = ({ logged }: navbarProps) => {
       </li>
     </ul>
   ) : (
-    <ul>
-      <li>
-        <NavLink to="/" className="navbar__link">
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/login" className="navbar__link">
-          Login
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/signup" className="navbar__link">
-          Signup
-        </NavLink>
-      </li>
-    </ul>
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div
+          className="font-bold text-2xl cursor-pointer flex content-center items-center font-[Poppins] 
+      text-gray-800"
+        >
+          <span className="text-2xl text-indigo-700 mr-3 ">
+            <FcLandscape name="logo-ionic"></FcLandscape>
+          </span>
+          <h1>Lets Meet Up </h1>
+        </div>
+
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+        >
+          <IoIosMenu name={open ? "close" : "menu"}></IoIosMenu>
+        </div>
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-20 opacity-100" : "top-[-490px]"
+          } md:opacity-100 opacity-100`}
+        >
+          <li className="md:ml-8 text-xl md:my-0 my-7">
+            <NavLink to="/" className="navbar__link">
+              <a className="text-gray-800 hover:text-gray-400 duration-500">
+                Home
+              </a>
+            </NavLink>
+          </li>
+          <li className="md:ml-8 text-xl md:my-0 my-7">
+            <NavLink to="/login" className="navbar__link">
+              <a className="text-gray-800 hover:text-gray-400 duration-500">
+                Login
+              </a>
+            </NavLink>
+          </li>
+          <li className="md:ml-8 text-xl md:my-0 my-7">
+            <NavLink to="/signup" className="navbar__link">
+              <a className="text-gray-800 hover:text-gray-400 duration-500">
+                Signup
+              </a>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 
   return <div className="navbar">{navLinks}</div>;

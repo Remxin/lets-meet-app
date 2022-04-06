eb3f010eb21fc5229036dd5da71c7a43a6fb1af6
Wiktor Orda <wiktor.orda@gmail.com>
2022-04-06 14:39:39 +0200

    fixed issue #4

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -69,7 +69,7 @@ const Navbar = ({ logged }: navbarProps) => {
         </div>
         <ul
           className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-20 opacity-100" : "top-[-490px]"
+            open ? "top-18 opacity-100" : "top-[-490px]"
           } md:opacity-100 opacity-100`}
         >
           <li className="md:ml-8 text-xl md:my-0 my-7">

f302dadf13a9851879aa563007c6dfef8f3efee0
Remxin <remxin7@gmail.com>
2022-09-27 09:27:06 +0200

    stable version

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 import "./App.scss";
-import "./styles/scss/fonts.scss"
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react'
+import "./styles/scss/fonts.scss";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import {
   ApolloClient,
   InMemoryCache,
@@ -50,7 +55,7 @@ const client = new ApolloClient({
 function App() {
   const [user, setUser] = useState<any>(null);
   const [isLogged, setIsLogged] = useState<boolean>(false);
-  const [isLoggingError, setIsLoggingError] = useState(false)
+  const [isLoggingError, setIsLoggingError] = useState(false);
   // const navigate = useNavigate()
 
   useEffect(() => {
@@ -61,7 +66,7 @@ function App() {
           `${process.env.REACT_APP_SERVER_IP}/verifyuser`,
           {
             method: "GET",
-            credentials: "include",// dołącza do zapytania cookiesy
+            credentials: "include", // dołącza do zapytania cookiesy
             headers: {
               "Content-Type": "application/json",
               Cache: "no-cache",
@@ -73,11 +78,11 @@ function App() {
         // })
         const data = await res.json();
         console.log(data);
-        
+
         await setUser(data);
       } catch (err) {
         console.log(err);
-        setIsLoggingError(true)
+        setIsLoggingError(true);
       }
     };
     verifyUser();
@@ -91,51 +96,61 @@ function App() {
     }
   }, [user]);
 
-
   return (
     // @ts-ignore
     <div className="app">
       <Router>
         <NextUIProvider>
           <ChakraProvider>
-          <ApolloProvider client={client}>
-            {/* @ts-ignore */}
-            <UserContext.Provider value={{ user, setUser }}>
-              <Navbar logged={isLogged} />
-              <div className="main">
-                <Routes>
-                  <Route path="/login" element={<NewLogin />} />
-                  <Route path="/forgot-password" element={<ForgotPassword />} />
-                  <Route path="/reset/password" element={<ResetPassword />} />
-                  {isLogged ?
-                  <>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/user" element={<UserPanel />} />
-                  {/* <Route path="/privacy-policy" element={<PrivacyPolicy />} /> */}
-                  <Route path="/add/event" element={<AddEvent />} />
-                  <Route path="/add/place" element={<AddPlace />} />
-                  <Route path="/events" element={<Events />} />
-                  <Route path="/myevents" element={<MyEvents/>} />
-                  <Route path="/event/:eventId" element={<Event />} />
-                  <Route path="/places" element={<VerifiedPlaces/>} />
-                  <Route path="/places/:placeId" element={<VerifiedPlace/>} />
-                  <Route path="/chats" element={<Chats />} />
-                  <Route path="*" element={<Page404 />} />
-                  </> : null }
-                  { isLoggingError ? 
-                  <>
-                  <Route path="*" element={<Navigate to="/login"/>}/>
-                  </> : null
-                  }
-                  { user?.role === "admin" ? <>
-                    <Route path="/admin" element={<AdminPanel />} />
-                    <Route path="/admin/unverifiedPlace/:placeId" element={<UnverifiedPlace />} />
-                  </> : null
-                  }
-                </Routes>
-              </div>
-            </UserContext.Provider>
-          </ApolloProvider>
+            <ApolloProvider client={client}>
+              {/* @ts-ignore */}
+              <UserContext.Provider value={{ user, setUser }}>
+                <Navbar logged={isLogged} />
+                <div className="main">
+                  <Routes>
+                    <Route path="/login" element={<NewLogin />} />
+                    <Route
+                      path="/forgot-password"
+                      element={<ForgotPassword />}
+                    />
+                    <Route path="/reset/password" element={<ResetPassword />} />
+                    {isLogged ? (
+                      <>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/user" element={<UserPanel />} />
+                        {/* <Route path="/privacy-policy" element={<PrivacyPolicy />} /> */}
+                        <Route path="/add/event" element={<AddEvent />} />
+                        <Route path="/add/place" element={<AddPlace />} />
+                        <Route path="/events" element={<Events />} />
+                        <Route path="/myevents" element={<MyEvents />} />
+                        <Route path="/event/:eventId" element={<Event />} />
+                        <Route path="/places" element={<VerifiedPlaces />} />
+                        <Route
+                          path="/places/:placeId"
+                          element={<VerifiedPlace />}
+                        />
+                        <Route path="/chats" element={<Chats />} />
+                        <Route path="*" element={<Page404 />} />
+                      </>
+                    ) : null}
+                    {isLoggingError ? (
+                      <>
+                        <Route path="*" element={<Navigate to="/login" />} />
+                      </>
+                    ) : null}
+                    {user?.role === "admin" ? (
+                      <>
+                        <Route path="/admin" element={<AdminPanel />} />
+                        <Route
+                          path="/admin/unverifiedPlace/:placeId"
+                          element={<UnverifiedPlace />}
+                        />
+                      </>
+                    ) : null}
+                  </Routes>
+                </div>
+              </UserContext.Provider>
+            </ApolloProvider>
           </ChakraProvider>
         </NextUIProvider>
       </Router>
